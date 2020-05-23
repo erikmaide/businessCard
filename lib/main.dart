23e9e764baa3ab9d6a6d41f6fd62c226d498d0f7
Erik Maide <maide.erik@gmail.com>
2020-05-23 21:37:41 +0300

    added SafeArea container and alignments to following childs

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: Center(
+            child: Container(
+                color: Colors.lightBlue,
+                child: Text('Erik Maide', textAlign: TextAlign.center),
+                height: 125.0,
+                width: 125.0,
+                padding: EdgeInsets.all(42.0)),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
